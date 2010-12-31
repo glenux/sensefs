aa60070d6a25f971d4ead09d182dc8ee936dec32
Glenn Y. Rolland <glenux@gmail.com>
2010-12-31 02:49:57 +0100

    Initial import.

---
+++ sensefs/stat.rb
@@ -0,0 +1,20 @@
+class Stat
+  attr_accessor :uid,:gid,:mode,:size,:atime,:mtime,:ctime 
+  attr_accessor :dev,:ino,:nlink,:rdev,:blksize,:blocks
+  def initialize
+    @uid     = 0
+    @gid     = 0
+    @mode    = 0
+    @size    = 0
+    @atime   = 0
+    @mtime   = 0
+    @ctime   = 0
+    @dev     = 0
+    @ino     = 0
+    @nlink   = 0
+    @rdev    = 0
+    @blksize = 0
+    @blocks  = 0
+  end
+end
+
